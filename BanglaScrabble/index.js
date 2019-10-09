78ceed0e64980fc2db56ddff3d2f645f74a40c68
Ahnaf93 <ahnafkhantulon@gmail.com>
2019-10-09 14:28:53 +0600

    Project Setup

---
+++ BanglaScrabble/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import createAppContainer from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => createAppContainer);
