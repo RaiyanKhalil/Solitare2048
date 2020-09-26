1c5fe2c34f4ce3b439f5dd3800161ef01ae83d80
tansenSiraji <tansenananda@gmail.com>
2020-09-27 01:24:05 +0600

    ABOUT SECTION ADDED

--- info.js
+++ info.js
@@ -1,37 +1,137 @@
-import React, { Component, useState, useEffect,  } from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
+import React, { Component } from "react";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from "react-native";
 
-import {StyleSheet, View, FlatList, ActivityIndicator, Image, TouchableOpacity, Alert, Button, Text, Dimensions, BackHandler} from 'react-native';
-import { NavigationEvents } from 'react-navigation';
+class info extends Component {
+  static navigationOptions = ({navigation}) => ({
+    // headerTitleStyle: { 
+    //   textAlign:"center", 
+    //   flex:1,
+    //   },
+      headerStyle: {
+        backgroundColor: '#FFFFFF',
+      },
 
-export default class info extends Component {
-    render(){
-        const {navigate} = this.props.navigation
-      
-          return(
-              <View>
-                  <TouchableOpacity>
-                      <View>
-                      <Text style = {{color: '#000000', fontSize: 30, textAlignVertical: 'center', fontWeight: 'bold', textAlignVertical: "center", }}>CONTINUE</Text>
-                      </View>
-                  </TouchableOpacity>
-                        <View>
-                          <TouchableOpacity
-                            
-      
-                         onPress={() => {
-                          navigate(
-                            "Game"
-                          )                     
-                         }
-                         
-                        }
-                        >
-                          <Text style = {{color: '#000000', fontSize: 30, textAlignVertical: 'center', fontWeight: 'bold', textAlignVertical: "center", }}>CONTINUE</Text>
-                        </TouchableOpacity>
-                        </View>
-                        
-              </View>
-          )
-      }
+    })
+  state = {
+    modalVisible: false
+  };
+
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
+  }
+
+  render() {
+    const { modalVisible } = this.state;
+    return (
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: "#FFB6C1", borderWidth: 5 }}
+                onPress={() => {
+                  this.setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle2}>Close</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+        <View style = {{ paddingBottom: 50}}>
+        <TouchableHighlight
+          style={styles.openButton}
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
+        >
+          <Text style={styles.textStyle}>About Us</Text>
+        </TouchableHighlight>
+        </View>
+        <View>
+        <TouchableHighlight
+          style={styles.openButton}
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
+        >
+          <Text style={styles.textStyle}>About Game</Text>
+        </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#000000",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    height: 50,
+    width: 200
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 20
+
+  },
+  textStyle2: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 20,
+    textAlignVertical: 'center',
+
+
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    height: 250,
+    width: 250,
+    //textAlignVertical: 'center',
+  }
+});
+
+export default info;
